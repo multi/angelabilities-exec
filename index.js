93d194f69efb4a796444859ad51607e1506fc552
Georgi Yonchev <georgi@yonchev.org>
2017-07-21 11:29:12 +0300

    preserve color output, add option to specify maxBuffer from ENV variable

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
-var exec = require("child_process").exec
+module.exports = function (angel) {
+  var exec = require('child_process').exec
 
-module.exports = function(angel){
-  angel.sh = function(command, next) {
-    var cwd = process.cwd();
-    var child = exec(command, { cwd: cwd, maxBuffer: 2000 * 1024 })
+  angel.sh = function (command, next) {
+    var cwd = process.cwd()
+    var child = exec(command, {
+      cwd: cwd,
+      maxBuffer: process.env.NODE_EXEC_MAX_BUFFER || 5 * 1024 * 1024,
+      env: Object.assign({}, process.env, {
+        FORCE_COLOR: true,
+      })
+    })
     child.stderr.pipe(process.stderr)
     child.stdout.pipe(process.stdout)
-    child.on("exit", function(code){
-      var err = null
-      if(code != 0)
-        err = new Error("failed "+command+" on "+cwd)
-      next && next(err)
+    child.on('exit', function (code) {
+      if (code !== 0) {
+        var err = new Error([
+          'failed',
+          command,
+          'on',
+          cwd
+        ].join(' '))
+
+        if (next) {
+          return next(err)
+        }
+
+        console.error(err)
+      }
+
+      if (next) {
+        next()
+      }
     })
+
     return child
   }
 }
